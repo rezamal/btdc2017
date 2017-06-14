d10069067aaeddf87520c5396785ebd3c7976f6d
rezamal <rezamal.ilham@gmail.com>
2017-06-14 20:00:19 +0700

    hello

---
+++ app/src/main/java/com/ilham/rezamal/bdt2017/DashboardActivity.java
@@ -0,0 +1,18 @@
+package com.ilham.rezamal.bdt2017;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenovo on 14/06/2017.
+ */
+
+public class DashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+    }
+}
+
--- app/src/main/java/com/ilham/rezamal/bdt2017/MainActivity.java
+++ app/src/main/java/com/ilham/rezamal/bdt2017/MainActivity.java
@@ -1,12 +1,16 @@
 package com.ilham.rezamal.bdt2017;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
