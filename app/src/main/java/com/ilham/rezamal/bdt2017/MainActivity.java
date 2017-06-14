fa78491ca57d27559116e71cab1b9e70265af1a7
rezamal <rezamal.ilham@gmail.com>
2017-06-14 18:29:25 +0700

    commit pertama

---
+++ app/src/main/java/com/ilham/rezamal/bdt2017/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ilham.rezamal.bdt2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
